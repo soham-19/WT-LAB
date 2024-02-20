4fa287109c96f296aaab36f986405709836bfbd4
SOham <114175483+soham-19@users.noreply.github.com>
2024-02-20 09:28:23 +0000

    20 feb

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require("path");
+const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate")
+
+const app = express()
+
+const PORT = 8080
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+app.use(express.static(path.join(__dirname, "/public")))
+
+app.engine("ejs", ejsMate)
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening at PORT ${PORT}`)
+})
