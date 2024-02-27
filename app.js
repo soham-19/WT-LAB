f7d7bb57c75ecd10bebc7971c322a8252239afba
SOham <114175483+soham-19@users.noreply.github.com>
2024-02-27 09:27:29 +0000

    27 feb

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const PORT = 8080
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(express.static(path.join(__dirname, "/public")))
@@ -19,6 +18,14 @@ app.engine("ejs", ejsMate)
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
+});
+
+app.get("/profile", (req, res) => {
+    res.render("profile.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
 });
 
 app.listen(PORT, () => {
